c19ee5a123fc0fdca3a96dcd75a44a67edaf8337
Flavio Pace <flavio.pace@gmail.com>
2017-10-28 19:21:45 +0200

    Add also VPN IP

--- openVPNparser.py
+++ openVPNparser.py
@@ -6,7 +6,7 @@ with open('/etc/openvpn/openvpn-status.log') as logfile:
 indexArr = 0
 arraycolor = ['0xeeeeee','0xdddddd']
 
-table  = ("<html><table style='font-weight: bold' bgcolor='B0DC7A' border='1'> <tr> <th>IP</th> <th>Cert Name</th> <th>Rcv Bytes</th> \
+table  = ("<html><table style='font-weight: bold' bgcolor='B0DC7A' border='1'> <tr> <th>Cert Name</th> <th>Real IP</th> <th>VPN IP</th> <th>Rcv Bytes</th> \
 <th>Sent Bytes</th> <th>Connected Since</th> </tr>")
 
 print(" - Date {}".format(status.updated_at))  # datetime.datetime(2015, 6, 18, 8, 12, 15)
@@ -23,8 +23,14 @@ def printDetails(ipaddr, client):
 
 def addHTMLRowTable(ipaddr, client, inc):
     table = "<tr bgcolor='{}'>". format(arraycolor[inc])
-    table += "<th>{}</th>". format(ip)
     table += "<th>{}</th>". format(client.common_name)
+    table += "<th>{}</th>". format(ip)
+    virtul_ip = ''
+    for  vclient, routing in status.routing_table.iteritems():
+        if( routing.common_name == client.common_name):
+            virtul_ip += str(routing.virtual_address)
+
+    table += "<th>{}</th>". format(virtul_ip)
     table += "<th>{}</th>". format(client.bytes_received)
     table += "<th>{}</th>". format(client.bytes_sent)
     table += "<th>{}</th>". format(client.connected_since)
