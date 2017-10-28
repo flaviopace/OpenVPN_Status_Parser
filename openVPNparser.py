2e05cdba0b94b76b86ddb2312fb6b88ae1cd5050
flavio.pace <flavio.pace@gmail.com>
2017-10-28 10:38:14 +0000

    starting add fn

--- openVPNparser.py
+++ openVPNparser.py
@@ -5,17 +5,19 @@ with open('/etc/openvpn/openvpn-status.log') as logfile:
 
 print(status.updated_at)  # datetime.datetime(2015, 6, 18, 8, 12, 15)
 
-#def printDetails(ipaddr, client):
+def printDetails(ipaddr, client):
 
-
-
-for ip, client in status.client_list.iteritems():
 	print "Client IP: {} " . format(ip)
 	print "Cert Name: {}" .format(client.common_name)
 	print "Client Rcv Bytes: {}" .format(client.bytes_received)
 	print "Client Snd Bytes: {}" .format(client.bytes_sent)
 	print "Client Connected: {}" .format(client.connected_since)
 
+
+
+
+for ip, client in status.client_list.iteritems():
+	printDetails(ip, client)	
 
 
 
