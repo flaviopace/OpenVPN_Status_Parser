3a1a966c8aa8b4919411279429d30242eb7c1dd7
Flavio Pace <flavio.pace@gmail.com>
2017-10-28 14:27:51 +0200

    Add HTML format output

--- openVPNparser.py
+++ openVPNparser.py
@@ -3,6 +3,9 @@ from openvpn_status import parse_status
 with open('/etc/openvpn/openvpn-status.log') as logfile:
     status = parse_status(logfile.read())
 
+table  = ("<html><table border='1'<tr> <th>IP</th> <th>Cert Name</th> <th>Rcv Bytes</th> \
+<th>Sent Bytes</th> <th>Connected Sinze</th> </tr>")
+
 print(status.updated_at)  # datetime.datetime(2015, 6, 18, 8, 12, 15)
 
 def printDetails(ipaddr, client):
@@ -15,6 +18,20 @@ def printDetails(ipaddr, client):
     print "------"
 
 
+def addHTMLRowTable(ipaddr, client):
+    table = "<tr>"
+    table += "<th>{}</th>". format(ip)
+    table += "<th>{}</th>". format(client.common_name)
+    table += "<th>{}</th>". format(client.bytes_received)
+    table += "<th>{}</th>". format(client.bytes_sent)
+    table += "<th>{}</th>". format(client.connected_since)
+    table += "</tr>"
+    return table
 
 for ip, client in status.client_list.iteritems():
-    printDetails(ip, client)
+    #printDetails(ip, client)
+    table += addHTMLRowTable(ip, client)
+
+table += ("</table></html>")
+
+print table
